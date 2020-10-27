25b2f64ca5411414cb1136f21f28423e618a8ced
masal9pse <returnymgstokh@icloud.com>
2020-10-28 02:09:41 +0900

    カテゴリー検索機能に使用するであろうSQL

--- db/exec_sql/exec.sql
+++ db/exec_sql/exec.sql
@@ -1,5 +1,22 @@
+-- カテゴリー検索の未完成例
 SELECT posts.* , categories.*
 from categories
  inner join posts
  on categories.id = posts.category_id
 where posts.category_id=2;
+
+-- 3つのテーブルの結合　カテゴリー検索機能
+SELECT
+ *
+FROM
+ posts
+ LEFT JOIN
+ post_category
+ ON
+posts.id = post_category.post_id
+ LEFT JOIN
+ categories
+ ON
+categories.id = post_category.category_id
+WHERE
+posts.id=3;
