62ba780dd665601196561302c7173d77a661925d
masal9pse <returnymgstokh@icloud.com>
2020-10-29 04:00:55 +0900

    formの未入力場合に、該当のsqlを発行しない

--- search.php
+++ search.php
@@ -1,26 +1,35 @@
 <?php
 require('dbconnect.php');
 //var_dump($_GET);
-if (!empty($_GET['category'] || $_GET['search'])) {
-
- $sql = 'SELECT * FROM posts
- LEFT JOIN
+$where = [];
+if (!empty($_GET['category'])) {
+ $where[] = 'LEFT JOIN
  post_category
  ON posts.id = post_category.post_id
  LEFT JOIN categories
  ON categories.id = post_category.category_id
-WHERE categories.category = :category and posts.title like :title or posts.detail like :detail';
+WHERE categories.category = :category';
 
- $stmt = $db->prepare($sql);
- $stmt->bindValue(':category', $_GET["category"], PDO::PARAM_STR);
- $stmt->bindValue(':title', '%' . $_GET['search'] . '%', PDO::PARAM_STR);
- $stmt->bindValue(':detail', '%' . $_GET['search'] . '%', PDO::PARAM_STR);
- $stmt->execute();
- $search = $stmt->fetchAll(PDO::FETCH_ASSOC);
- //var_dump($stmt);
- var_dump($search);
+ //$stmt = $db->prepare($sql);
+ //$stmt->bindValue(':category', $_GET["category"], PDO::PARAM_STR);
+ //$stmt->bindValue(':title', '%' . $_GET['search'] . '%', PDO::PARAM_STR);
+ //$stmt->bindValue(':detail', '%' . $_GET['search'] . '%', PDO::PARAM_STR);
+ //$stmt->execute();
+ //$search = $stmt->fetchAll(PDO::FETCH_ASSOC);
+ ////var_dump($stmt);
+ //var_dump($search);
+ //var_dump($where);
+}
+if (!empty($_GET['search'])) {
+ $where[] = 'posts.title like :title or posts.detail like :detail';
 }
 
+//var_dump($where);
+
+if (isset($where)) {
+ $whereSql = implode(' AND ', $where);
+}
+var_dump($whereSql);
 //if (!empty($_GET['search'])) {
 // $sql = 'SELECT * from posts where title like :title or detail like :detail';
 // //$sql = 'SELECT * from posts where title=:title';
