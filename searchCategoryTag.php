a77807418f2adbef9b3b810d9ae431ef45cd04ff
masal9pse <returnymgstokh@icloud.com>
2020-10-29 18:22:40 +0900

    sqlの可読性をあげる

--- searchCategoryTag.php
+++ searchCategoryTag.php
@@ -3,17 +3,10 @@ require('dbconnect.php');
 //var_dump($_GET);
 
 if (!empty($_GET['category'] && empty($_GET['search']))) {
- $sql = 'SELECT * FROM posts
-  LEFT JOIN
-  post_category
-  ON
- posts.id = post_category.post_id
-  LEFT JOIN
-  categories
-  ON
- categories.id = post_category.category_id
- WHERE
- categories.category = :category';
+ $sql = 'SELECT * FROM posts LEFT JOIN post_category ON posts.id = post_category.post_id
+ LEFT JOIN categories
+ ON categories.id = post_category.category_id
+WHERE categories.category = :category';
  $stmt = $db->prepare($sql);
  $stmt->bindValue(':category', $_GET["category"], PDO::PARAM_STR);
  $stmt->execute();
@@ -37,41 +30,16 @@ if (empty($_GET['category']) && !empty($_GET['search'])) {
 }
 
 if (!empty($_GET['category'] && $_GET['search'])) {
- $sql = 'SELECT * FROM posts
- LEFT JOIN
- post_category
- ON
-posts.id = post_category.post_id
- LEFT JOIN
- categories
- ON
-categories.id = post_category.category_id
-WHERE
-categories.category = :category and posts.title like :title or posts.detail like :detail';
+ $sql = 'SELECT * FROM posts LEFT JOIN post_category ON posts.id = post_category.post_id
+ LEFT JOIN categories
+ ON categories.id = post_category.category_id
+WHERE categories.category = :category and posts.title like :title or posts.detail like :detail';
  $stmt = $db->prepare($sql);
  $stmt->bindValue(':category', $_GET['category'], PDO::PARAM_STR);
  $stmt->bindValue(':title', '%' . $_GET['search'] . '%', PDO::PARAM_STR);
  $stmt->bindValue(':detail', '%' . $_GET['search'] . '%', PDO::PARAM_STR);
- var_dump($stmt);
+ //var_dump($stmt);
  $stmt->execute();
  $results = $stmt->fetchAll(PDO::FETCH_ASSOC);
  var_dump($results);
 }
-
-//if (!empty($_GET['category'] && $_GET['search'] && $_GET['tag'])) {
-// $sql = 'SELECT * from posts　LEFT JOIN
-// post_category
-// ON posts.id = post_category.post_id
-// LEFT JOIN categories
-// ON categories.id = post_category.category_id
-// ON tags.id = post_tag.tag_id
-// LEFT JOIN post_category
-// ON posts.id = post_category.post_id
-// LEFT JOIN categories
-// ON categories.id = post_category.category_id
-//WHERE categories.category = :category 
-//and 
-//posts.title like :title or posts.detail like :detail
-//and  tags.tag = :tag';
-//}
-//var_dump($sql);
