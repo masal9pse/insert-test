5b129e04f74b9a552a219d2ac6ea3fdd6e8b3974
masal9pse <returnymgstokh@icloud.com>
2020-10-29 15:16:54 +0900

    カテゴリーを冗長な書き方で取得

--- search2.php
+++ search2.php
@@ -4,41 +4,37 @@ require('dbconnect.php');
 //$where = [];
 //$binds = [];
 //if (!empty($_GET['category']) && empty($_GET['search'])) {
-if (!empty($_GET['category']) && isset($_GET['search'])) {
- $sql = 'SELECT * from posts　LEFT JOIN
- post_category
- ON posts.id = post_category.post_id
- LEFT JOIN categories
- ON categories.id = post_category.category_id
-WHERE categories.category = :category';
+if (!empty($_GET['category'])) {
+ $sql = 'SELECT * FROM posts
+  LEFT JOIN
+  post_category
+  ON
+ posts.id = post_category.post_id
+  LEFT JOIN
+  categories
+  ON
+ categories.id = post_category.category_id
+ WHERE
+ categories.category = :category';
+ $stmt = $db->prepare($sql);
+ $stmt->bindValue(':category', $_GET["category"], PDO::PARAM_STR);
+ $stmt->execute();
+ $search2 = $stmt->fetchAll(PDO::FETCH_ASSOC);
+ var_dump($search2);
 }
-
-$stmt = $db->prepare($sql);
-$stmt->bindValue(':category', $_GET["category"], PDO::PARAM_STR);
-$stmt->execute();
-var_dump($stmt);
-//$results = $stmt->fetchAll(PDO::FETCH_ASSOC);
-$result2 = $stmt->fetchAll(PDO::FETCH_ASSOC);
 //var_dump($stmt);
-var_dump($result2);
-//if (!empty($results[0])) {
-// echo 'test_tableからidが取得できました';
-//} else {
-// echo 'test_tableからidを取得できませんでした';
-//}
-exit;
+//exit;
 
 if (!empty($_GET['search'])) {
- $sql = 'SELECT * from posts where posts.title like :title or posts.detail like :detail';
+ $sql = 'SELECT * FROM posts where posts.title like :title or posts.detail like :detail';
  $stmt = $db->prepare($sql);
  $stmt->bindValue(':title', '%' . $_GET['search'] . '%', PDO::PARAM_STR);
  $stmt->bindValue(':category', '%' . $_GET['search'] . '%', PDO::PARAM_STR);
  $stmt->execute();
- var_dump($stmt);
- $results = $stmt->fetchAll(PDO::FETCH_ASSOC);
- var_dump($results);
+ //var_dump($stmt);
+ //$results = $stmt->fetchAll(PDO::FETCH_ASSOC);
+ //var_dump($results);
 }
-
 if (!empty($_GET['category'] && $_GET['search'])) {
  $sql = 'SELECT * from posts　LEFT JOIN
  post_category
