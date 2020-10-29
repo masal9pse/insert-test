624b3397e026642f7e0e5f1a8f67e2e1a72f98d6
masal9pse <returnymgstokh@icloud.com>
2020-10-29 18:19:38 +0900

    おそらく全角スペースが入っていたので動かなかった

--- db/exec_sql/tag_search.sql
+++ db/exec_sql/tag_search.sql
@@ -33,3 +33,21 @@ FROM posts
  LEFT JOIN categories
  ON categories.id = post_category.category_id
 WHERE tags.tag = '面白い' and categories.category='アニメ' AND posts.title like '%N%' or posts.detail like '%N%';
+
+-- 正常値 何も出力されない
+SELECT *
+from posts LEFT JOIN
+ post_category
+ ON posts.id = post_category.post_id
+ LEFT JOIN categories
+ ON categories.id = post_category.category_id
+WHERE categories.category = '音楽' and posts.title like '%N%' or posts.detail like '%N%';
+
+--　正常値、Narutoが出力される
+SELECT *
+from posts LEFT JOIN
+ post_category
+ ON posts.id = post_category.post_id
+ LEFT JOIN categories
+ ON categories.id = post_category.category_id
+WHERE categories.category = '映画' and posts.title like '%N%' or posts.detail like '%N%';
--- searchCategoryTag.php
+++ searchCategoryTag.php
@@ -2,7 +2,6 @@
 require('dbconnect.php');
 //var_dump($_GET);
 
-//if (!empty($_GET['category']) && empty($_GET['search'])) {
 if (!empty($_GET['category'] && empty($_GET['search']))) {
  $sql = 'SELECT * FROM posts
   LEFT JOIN
@@ -24,6 +23,7 @@ if (!empty($_GET['category'] && empty($_GET['search']))) {
 //var_dump($stmt);
 //exit;
 if (empty($_GET['category']) && !empty($_GET['search'])) {
+ //if (!empty($_GET['search'])) {
  $sql = 'SELECT * FROM posts 
  WHERE posts.title LIKE :title OR posts.detail LIKE :detail';
  $stmt = $db->prepare($sql);
@@ -37,16 +37,22 @@ if (empty($_GET['category']) && !empty($_GET['search'])) {
 }
 
 if (!empty($_GET['category'] && $_GET['search'])) {
- $sql = 'SELECT * from posts　LEFT JOIN
+ $sql = 'SELECT * FROM posts
+ LEFT JOIN
  post_category
- ON posts.id = post_category.post_id
- LEFT JOIN categories
- ON categories.id = post_category.category_id
-WHERE categories.category = :category and posts.title like :title or posts.detail like :detail';
+ ON
+posts.id = post_category.post_id
+ LEFT JOIN
+ categories
+ ON
+categories.id = post_category.category_id
+WHERE
+categories.category = :category and posts.title like :title or posts.detail like :detail';
  $stmt = $db->prepare($sql);
  $stmt->bindValue(':category', $_GET['category'], PDO::PARAM_STR);
  $stmt->bindValue(':title', '%' . $_GET['search'] . '%', PDO::PARAM_STR);
- $stmt->bindValue(':category', '%' . $_GET['search'] . '%', PDO::PARAM_STR);
+ $stmt->bindValue(':detail', '%' . $_GET['search'] . '%', PDO::PARAM_STR);
+ var_dump($stmt);
  $stmt->execute();
  $results = $stmt->fetchAll(PDO::FETCH_ASSOC);
  var_dump($results);
