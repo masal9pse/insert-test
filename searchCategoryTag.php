6762d082f21a4c01d41fb4750fbc3b6879bf43fa
masal9pse <returnymgstokh@icloud.com>
2020-11-01 00:31:37 +0900

    タグ以外が入力されているときの条件で発生するバグを修正

--- searchCategoryTag.php
+++ searchCategoryTag.php
@@ -36,7 +36,13 @@ if (!empty($_GET['tags'] && $_GET['category']) && empty($_GET['search'])) {
 
 // tag,category,searchの絞り込み検索
 if (!empty($_GET['tags'] && $_GET['search'] && $_GET['category'])) {
- //if (!empty($_GET['tags'] && $_GET['category'])) {
+ $category_count = count($_GET['tags']);
+ $where = [];
+ foreach ($_GET['tags'] as $tag) {
+  $where[] = "'$tag'";
+ }
+ $whereSql = implode(' , ', $where);
+ //var_dump($where);
  $sql = "SELECT count(*), posts.*
  FROM posts
   LEFT JOIN post_category
@@ -47,11 +53,12 @@ if (!empty($_GET['tags'] && $_GET['search'] && $_GET['category'])) {
   ON posts.id = post_tag.post_id
   JOIN tags
   ON post_tag.tag_id = tags.id
- WHERE  categories.category = 'アニメ'
-  AND tags.tag IN ('感動できる','面白い')
-  AND posts.title like '%N%' or posts.detail like '%N%'
+ WHERE  categories.category = '{$_GET['category']}'
+  AND tags.tag IN ($whereSql)  
+  AND posts.title like '%{$_GET['search']}%' OR posts.detail like '%{$_GET['search']}%'
  GROUP BY posts.id
- HAVING COUNT(posts.id) = 2";
+ HAVING COUNT(posts.id) = $category_count";
+
  var_dump($sql);
  $stmt = $db->query($sql);
  $tag_search = $stmt->fetchAll(PDO::FETCH_ASSOC);
@@ -66,7 +73,8 @@ WHERE pt.tag_id = t.id
  AND (t.tag IN (";
 
  $second_sql = "AND p.id = pt.post_id
- AND p.title LIKE '%{$_GET['search']}%' or p.detail LIKE '%{$_GET['search']}%'
+ AND p.title LIKE '%{$_GET['search']}%' 
+ OR p.detail LIKE '%{$_GET['search']}%'
 GROUP BY p.id
 HAVING COUNT( p.id )= ";
 
@@ -146,10 +154,15 @@ if (!empty($_GET['search']) && empty($_GET['category']) && empty($_GET['tags']))
 }
 
 if (!empty($_GET['category'] && $_GET['search']) && empty($_GET['tags'])) {
- $sql = 'SELECT * FROM posts LEFT JOIN post_category ON posts.id = post_category.post_id
+ $sql = 'SELECT distinct posts.* FROM posts 
+ LEFT JOIN post_category 
+ ON posts.id = post_category.post_id
  LEFT JOIN categories
  ON categories.id = post_category.category_id
-WHERE categories.category = :category and posts.title like :title or posts.detail like :detail';
+WHERE categories.category = :category 
+AND posts.title like :title 
+OR posts.detail like :detail';
+
  $stmt = $db->prepare($sql);
  $stmt->bindValue(':category', $_GET['category'], PDO::PARAM_STR);
  $stmt->bindValue(':title', '%' . $_GET['search'] . '%', PDO::PARAM_STR);
