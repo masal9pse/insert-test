37ef53c0ce68810615d0e67fc726c6a7011a8bcf
masal9pse <returnymgstokh@icloud.com>
2020-11-01 02:12:34 +0900

    更新画面に画像を表示

--- update_form.php
+++ update_form.php
@@ -7,3 +7,24 @@ $stmt->bindValue(':id', $_GET['id'], PDO::PARAM_INT);
 $stmt->execute();
 $update_post = $stmt->fetch(PDO::FETCH_ASSOC);
 var_dump($update_post);
+?>
+
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+ <meta charset="UTF-8">
+ <meta name="viewport" content="width=device-width, initial-scale=1.0">
+ <title>更新画面</title>
+</head>
+
+<body>
+ <form action="update.php" method="post">
+  <p><?php echo $update_post['title']; ?></p>
+  <p><?php echo $update_post['detail']; ?></p>
+  <img src="<?php echo 'images/' . $update_post['image'] ?>" alt="">
+  <input type="submit" value="編集する">
+ </form>
+</body>
+
+</html>
