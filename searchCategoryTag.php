c56b4bc7deca346b4d9bc87a2dd8a88de1ae62b8
masal9pse <returnymgstokh@icloud.com>
2020-10-31 16:24:34 +0900

    検索時に発行するsqlの条件分岐、冗長すぎるのであとで修正する

--- searchCategoryTag.php
+++ searchCategoryTag.php
@@ -2,8 +2,34 @@
 require('dbconnect.php');
 //var_dump(count($_GET['tags']));
 //echo count($_GET['tags']);
-//exit;
+
+// tagとカテゴリーの絞り込み検索
+if (!empty($_GET['tags'] && $_GET['category']) && empty($_GET['search'])) {
+ //if (!empty($_GET['tags'] && $_GET['category'])) {
+ $sql = "SELECT count(*), posts.*
+ FROM posts
+  LEFT JOIN post_category
+  ON posts.id = post_category.post_id
+  LEFT JOIN categories
+  ON categories.id = post_category.category_id
+  JOIN post_tag
+  ON posts.id = post_tag.post_id
+  JOIN tags
+  ON post_tag.tag_id = tags.id
+ WHERE  categories.category = 'アニメ'
+  AND tags.tag IN ('感動できる','面白い')  
+ GROUP BY posts.id
+ HAVING COUNT(posts.id) = 2";
+ var_dump($sql);
+ $stmt = $db->query($sql);
+ $tag_search = $stmt->fetchAll(PDO::FETCH_ASSOC);
+ var_dump($tag_search);
+ //exit;
+}
+
+// tag,category,searchの絞り込み検索
 if (!empty($_GET['tags'] && $_GET['search'] && $_GET['category'])) {
+ //if (!empty($_GET['tags'] && $_GET['category'])) {
  $sql = "SELECT count(*), posts.*
  FROM posts
   LEFT JOIN post_category
@@ -19,13 +45,14 @@ if (!empty($_GET['tags'] && $_GET['search'] && $_GET['category'])) {
   AND posts.title like '%N%' or posts.detail like '%N%'
  GROUP BY posts.id
  HAVING COUNT(posts.id) = 2";
+ var_dump($sql);
  $stmt = $db->query($sql);
  $tag_search = $stmt->fetchAll(PDO::FETCH_ASSOC);
  var_dump($tag_search);
- exit;
+ //exit;
 }
 
-if (!empty($_GET['tags'] && $_GET['search'])) {
+if (!empty($_GET['tags'] && $_GET['search']) && empty($_GET['category'])) {
  $first_sql = "SELECT p.*
 FROM post_tag pt, posts p, tags t
 WHERE pt.tag_id = t.id
@@ -53,7 +80,7 @@ HAVING COUNT( p.id )= ";
  var_dump($tag_search);
 }
 
-if (!empty($_GET['tags']) && empty($_GET['search'])) {
+if (!empty($_GET['tags']) && empty($_GET['search']) && empty($_GET['tags'])) {
  $first_sql = "SELECT p.*
 FROM post_tag pt, posts p, tags t
 WHERE pt.tag_id = t.id
@@ -81,22 +108,9 @@ HAVING COUNT( p.id )= ";
  $stmt = $db->query($sql);
  $tag_search = $stmt->fetchAll(PDO::FETCH_ASSOC);
  var_dump($tag_search);
- exit;
- //var_dump($whereSql);
- //$stmt->bindValue(':tags', $tag, PDO::PARAM_STR);
- //$stmt->execute();
- //$tag_searches[] = $stmt->fetchAll(PDO::FETCH_ASSOC);
- //var_dump($tag_searches);
- foreach ($tag_searches as $tag_search) {
-  //var_dump($tag_search);
-  foreach ($tag_search as $result) {
-   //var_dump($result);
-   echo $result['title'] . ' ';
-  }
- }
 }
 //exit;
-if (!empty($_GET['category'] && empty($_GET['search']))) {
+if (!empty($_GET['category'] && empty($_GET['search']) && empty($_GET['tags']))) {
  $sql = 'SELECT * FROM posts LEFT JOIN post_category ON posts.id = post_category.post_id
  LEFT JOIN categories
  ON categories.id = post_category.category_id
@@ -104,12 +118,13 @@ WHERE categories.category = :category';
  $stmt = $db->prepare($sql);
  $stmt->bindValue(':category', $_GET["category"], PDO::PARAM_STR);
  $stmt->execute();
+ var_dump($sql);
  $search2 = $stmt->fetchAll(PDO::FETCH_ASSOC);
  var_dump($search2);
 }
 //var_dump($stmt);
 //exit;
-if (empty($_GET['category'] && $_GET['tags']) && !empty($_GET['search'])) {
+if (!empty($_GET['search']) && empty($_GET['category']) && empty($_GET['tags'])) {
  //if (!empty($_GET['search'])) {
  $sql = 'SELECT * FROM posts 
  WHERE posts.title LIKE :title OR posts.detail LIKE :detail';
@@ -123,7 +138,7 @@ if (empty($_GET['category'] && $_GET['tags']) && !empty($_GET['search'])) {
  //exit;
 }
 
-if (!empty($_GET['category'] && $_GET['search'])) {
+if (!empty($_GET['category'] && $_GET['search']) && empty($_GET['tags'])) {
  $sql = 'SELECT * FROM posts LEFT JOIN post_category ON posts.id = post_category.post_id
  LEFT JOIN categories
  ON categories.id = post_category.category_id
@@ -132,7 +147,7 @@ WHERE categories.category = :category and posts.title like :title or posts.detai
  $stmt->bindValue(':category', $_GET['category'], PDO::PARAM_STR);
  $stmt->bindValue(':title', '%' . $_GET['search'] . '%', PDO::PARAM_STR);
  $stmt->bindValue(':detail', '%' . $_GET['search'] . '%', PDO::PARAM_STR);
- //var_dump($stmt);
+ var_dump($stmt);
  $stmt->execute();
  $results = $stmt->fetchAll(PDO::FETCH_ASSOC);
  var_dump($results);
