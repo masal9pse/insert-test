e78f6a3e23725f7bdca0d818fa25a6f22c776870
masal9pse <returnymgstokh@icloud.com>
2020-11-02 06:41:52 +0900

    ユーザー投稿一覧表示機能実装

--- user_posts_form.php
+++ user_posts_form.php
@@ -1,5 +1,18 @@
 <?php
+//　/user_posts_form.php?id={num} でアクセスしてください
+include('dbconnect.php');
 $sql = 'SELECT * from posts 
 inner join users 
 on posts.user_id = users.id
-where users.id=1';
+where users.id=:user_id';
+
+$stmt = $db->prepare($sql);
+$stmt->bindValue(':user_id', $_GET['id'], PDO::PARAM_INT);
+//$stmt->bindValue(':user_id', 1, PDO::PARAM_INT);
+$stmt->execute();
+$results = $stmt->fetchAll(PDO::FETCH_ASSOC);
+//var_dump($results);
+foreach ($results as $result) {
+ echo $result['title'] . ' ';
+ echo $result['detail'] . ' ';
+}
