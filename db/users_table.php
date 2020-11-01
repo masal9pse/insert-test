8cc08e6c08fad1d41444e68c6a34f2ff90e8087a
masal9pse <returnymgstokh@icloud.com>
2020-11-02 06:12:27 +0900

    create Userstable

---
+++ db/users_table.php
@@ -0,0 +1,19 @@
+<?php
+try {
+ $db = new PDO('pgsql:dbname=offshoa_db;host=127.0.0.1;port=5432;', 'yamamotohiroto', '');
+} catch (PDOException $e) {
+ echo $e . 'エラーです';
+}
+
+// テーブル作成のSQLを作成
+$sql = 'CREATE TABLE users (
+ id SERIAL NOT NULL,
+ name VARCHAR(255) NOT NULL, 
+ password VARCHAR(255),
+ created_at TIMESTAMP,
+ updated_at TIMESTAMP,
+ PRIMARY KEY (id)
+)';
+
+// SQLを実行
+$res = $db->query($sql);
