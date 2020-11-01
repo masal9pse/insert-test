ce6c659831a939804559a798a6f21130e846ebb1
masal9pse <returnymgstokh@icloud.com>
2020-11-01 15:50:53 +0900

    画像更新の際、古い画像を削除

--- update.php
+++ update.php
@@ -1,17 +1,17 @@
 <?php
 include('dbconnect.php');
-var_dump($_POST);
+//var_dump($_POST);
 //var_dump($_FILES);
 
 try {
- //$select_sql = 'SELECT * from posts where id=:id';
- //$select_stmt = $db->prepare($sql);
- //$select_stmt->bindValue(':id', $_POST['id'], PDO::PARAM_INT);
- //$select_stmt->execute();
- //$update_post =  $select_stmt->fetch(PDO::FETCH_ASSOC);
- //var_dump($update_post);
+ $select_sql = 'SELECT * from posts where id=:id';
+ $select_stmt = $db->prepare($select_sql);
+ $select_stmt->bindValue(':id', $_POST['id'], PDO::PARAM_INT);
+ $select_stmt->execute();
+ $result_for_rm_old_image =  $select_stmt->fetch(PDO::FETCH_ASSOC);
+ var_dump($result_for_rm_old_image);
  //exit;
- $update_sql = 'UPDATE posts set title=:title,detail=:detail,image=:image created_at=now(),updated_at=now() where id=:id';
+ $update_sql = 'UPDATE posts set title=:title,detail=:detail,image=:image,created_at=now(),updated_at=now() where id=:id';
  $image = uniqid(mt_rand(), true); //ファイル名をユニーク化
  //var_dump($image);
  //$image .= '.' . substr(strrchr($_FILES['image']['name'], '.'), 1);
@@ -24,12 +24,10 @@ try {
  $update_stmt->bindValue(':detail', $_POST['detail'], PDO::PARAM_STR);
  $update_stmt->bindValue(':image', $image, PDO::PARAM_STR);
  if (!empty($_FILES['image']['name'])) {
-  unlink('images/' . $update_post['image']);
+  unlink('./images/' . $result_for_rm_old_image['image']);
   move_uploaded_file($_FILES['image']['tmp_name'], './images/' . $image);
  }
  $update_stmt->execute();
- $result = $update_stmt->fetch();
- var_dump($result);
  echo '更新にせいこうしました';
 } catch (PDOException $e) {
  echo $e . '更新できませんでした';
