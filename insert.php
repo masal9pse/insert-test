76cd21b8c5c1f38167490ac1f16c213891caf244
masal9pse <returnymgstokh@icloud.com>
2020-11-03 02:00:41 +0900

    post_tagに複数挿入

--- insert.php
+++ insert.php
@@ -1,15 +1,14 @@
 <?php
 require('dbconnect.php');
 //var_dump($_FILES);
-var_dump($_POST);
-//foreach($tag as )
-$count_tags = count($_POST['tags']);
-var_dump($count_tags);
+//var_dump($_POST);
+//var_dump($count_tags);
 //exit;
 foreach ($_POST['tags'] as $tag) {
  var_dump($tag);
 }
 //exit;
+$count_tags = count($_POST['tags']);
 if (empty($_POST['title'])) {
  exit('タイトルを入力してください');
 }
@@ -18,7 +17,7 @@ $image .= '.' . substr(strrchr($_FILES['image']['name'], '.'), 1);
 //var_dump($image);
 //exit;
 $posts_sql = 'INSERT INTO posts(title,detail,image,created_at,updated_at) VALUES (:title,:detail,:image,now(),now())';
-$tag_sql = "INSERT INTO post_tag(post_id,tag_id) VALUES (3,6)";
+$tag_sql = "INSERT INTO post_tag(post_id,tag_id) VALUES (:post_id,:tag_id)";
 $db->beginTransaction();
 try {
  $post_stmt = $db->prepare($posts_sql);
@@ -31,19 +30,20 @@ try {
  // $tag_stmt->bindValue(':post_id', 3, PDO::PARAM_INT);
  // $tag_stmt->bindValue(':tag_id', $tag, PDO::PARAM_INT);
  //}
- //for ($i = 1; $i <= $count_tags; $i++) {
- //$tag_sql = "INSERT INTO post_tag VALUES(3,6)";
- //$tag_stmt = $db->query($tag_sql);
- //$tag_stmt = $db->prepare($tag_sql);
- //$tag_stmt->bindValue(':post_id', 3, PDO::PARAM_INT);
- //$tag_stmt->bindValue(':tag_id', $tag, PDO::PARAM_INT);
- //}
+ for ($i = 1; $i <= $count_tags; $i++) {
+  //$tag_sql = "INSERT INTO post_tag VALUES(3,6)";
+  //$tag_stmt = $db->query($tag_sql);
+  //var_dump($tag);
+  //exit;
+  $tag_stmt = $db->prepare($tag_sql);
+  $tag_stmt->bindValue(':post_id', 3, PDO::PARAM_INT);
+  $tag_stmt->bindValue(':tag_id', $tag, PDO::PARAM_INT);
+  $tag_stmt->execute();
+ }
  if (!empty($_FILES['image']['name'])) { //ファイルが選択されていれば$imageにファイル名を代入
   move_uploaded_file($_FILES['image']['tmp_name'], './images/' . $image);
  }
  $post_stmt->execute();
- $tag_stmt = $db->prepare($tag_sql);
- $tag_stmt->execute();
  $db->commit();
  echo '投稿に成功しました';
  echo "<img src=\" ./images/$image \">";
