3968e8e4bed33ad10b3d6775db474d818e2abbe5
masal9pse <returnymgstokh@icloud.com>
2020-11-03 15:12:24 +0900

    関数の処理の記載場所を変更

--- insert.php
+++ insert.php
@@ -1,16 +1,18 @@
 <?php
 //ini_set('display_errors', "On");
-require('dbconnect.php');
+include('dbconnect.php');
+include('util.php');
 $db = dbConnect();
+
 if (empty($_POST['title'])) {
  exit('タイトルを入力してください');
 }
-//var_dump($image);
-//exit;
+
 $db->beginTransaction();
 try {
  postInsert($db, $_POST);
  postTagInsert($db, $_POST);
+
  echo '投稿に成功しました';
  // 空の場合
  echo "<img src=\" ./images/$image \">";
--- util.php
+++ util.php
@@ -17,3 +17,34 @@ function getById($db, $id)
  $update_post = $stmt->fetch(PDO::FETCH_ASSOC);
  return $update_post;
 }
+
+// 記事投稿
+function postInsert($db, $post)
+{
+ $sql = 'INSERT INTO posts(title,detail,image,created_at,updated_at) VALUES (:title,:detail,:image,now(),now())';
+ $image = uniqid(mt_rand(), true);
+ $image .= '.' . substr(strrchr($_FILES['image']['name'], '.'), 1);
+ $stmt = $db->prepare($sql);
+ $stmt->bindValue(':image', $image, PDO::PARAM_STR);
+ $stmt->bindValue(':title', $post['title'], PDO::PARAM_STR);
+ $stmt->bindValue(':detail', $post['detail'], PDO::PARAM_STR);
+ if (!empty($_FILES['image']['name'])) {
+  move_uploaded_file($_FILES['image']['tmp_name'], './images/' . $image);
+ }
+ $stmt->execute();
+}
+
+// 記事に紐づいたタグを中間テーブルにインサート
+function postTagInsert($db, $tags)
+{
+ $sql = "INSERT INTO post_tag(post_id,tag_id) VALUES (:post_id,:tag_id)";
+ $now_post_insert_id = $db->lastInsertId();
+ var_dump($now_post_insert_id);
+ foreach ($tags['tags'] as $tag_num) {
+  $tag_stmt = $db->prepare($sql);
+  //var_dump($tag_num);
+  $tag_stmt->bindValue(':post_id', $now_post_insert_id, PDO::PARAM_INT);
+  $tag_stmt->bindValue(':tag_id', $tag_num, PDO::PARAM_INT);
+  $tag_stmt->execute();
+ }
+}
