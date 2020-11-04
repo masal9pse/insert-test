ca5e1dd7fffe90100c336b8b1055c76713d9c6d5
masal9pse <returnymgstokh@icloud.com>
2020-11-04 14:47:11 +0900

    phpunit導入

--- dbconnect.php
+++ dbconnect.php
@@ -2,7 +2,7 @@
 function dbConnect()
 {
  try {
-  //ini_set('display_errors', "On");
+  ini_set('display_errors', "On");
   $db = new PDO('pgsql:dbname=offshoa_db;host=127.0.0.1;port=5432;', 'yamamotohiroto', '');
   //echo '接続成功です';
  } catch (PDOException $e) {
