70803aabbd3b142e9812b5ce91a75b9be98bbf68
masal9pse <returnymgstokh@icloud.com>
2020-11-04 15:06:10 +0900

    update readme

--- dbconnect.php
+++ dbconnect.php
@@ -1,8 +1,9 @@
 <?php
+ini_set('display_errors', "On");
 function dbConnect()
 {
  try {
-  ini_set('display_errors', "On");
+  //ini_set('display_errors', "On");
   $db = new PDO('pgsql:dbname=offshoa_db;host=127.0.0.1;port=5432;', 'yamamotohiroto', '');
   //echo '接続成功です';
  } catch (PDOException $e) {
