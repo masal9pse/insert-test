479d525bd311ff5cdaf9975d89bba3229591cec9
masal9pse <returnymgstokh@icloud.com>
2020-11-04 17:27:29 +0900

    認証しているかしていないかで処理を変更

--- list.php
+++ list.php
@@ -20,9 +20,18 @@ var_dump($_SESSION);
  <h1>一覧リスト</h1>
  <a href="./search_form.php">検索リンク</a>
  <a href="./insert_form.php">投稿リンク</a>
- <form action="./auth/logout.php" method="post">
-  <button type="submit" name="logout" class="btn btn-danger">ログアウト</button>
- </form>
+
+ <?php if (empty($_SESSION['id'])) : ?>
+  <form action="./auth/login.php" method="get">
+   <button type="submit" class="btn btn-danger">ログイン</button>
+   <button type="submit" class="btn btn-danger">新規投稿</button>
+  </form>
+ <?php else : ?>
+  <form action="./auth/logout.php" method="post">
+   <button type="submit" name="logout" class="btn btn-danger">ログアウト</button>
+  </form>
+ <?php endif; ?>
+
  <?php foreach ($lists as $list) : ?>
   <div>
    <td><?php echo $list['title']; ?></td>
@@ -30,7 +39,9 @@ var_dump($_SESSION);
    <td><button type="button" onclick="location.href='./update_form.php?id=<?php print($list['id']) ?>'">編集</button></td>
   </div>
  <?php endforeach ?>
- <button type="button" onclick="location.href='./mypage.php?id=<?php echo $_SESSION['id'] ?>'">マイページ</button>
+ <?php if (!empty($_SESSION['id'])) : ?>
+  <button type="button" onclick="location.href='./mypage.php?id=<?php echo $_SESSION['id'] ?>'">マイページ</button>
+ <?php endif; ?>
 </body>
 
 </html>
