250e28e7bbf19320029effff20b41790b464566c
masal9pse <returnymgstokh@icloud.com>
2020-11-06 00:38:07 +0900

    いいね機能をマイページ画面でも動作

--- list.php
+++ list.php
@@ -6,76 +6,76 @@ $db = dbConnect();
 $lists = getAllData($db, 'posts');
 //echo $_SESSION['auth_id'];
 var_dump($_SESSION);
-if (empty($_SESSION)) {
- $_SESSION['auth_id'] = "名無しのごんべ";
+if (empty($_SESSION['auth_id'])) {
+  $_SESSION['auth_id'] = "名無しのごんべ";
 }
 //if (!empty($_POST['logout'])) {
 // $logout = $_POST['logout'];
 //}
 //if (isset($logout)) {
 if (isset($_POST['logout'])) {
- logout($_SESSION, 'list.php');
+  logout($_SESSION, 'list.php');
 }
 ?>
 <!DOCTYPE html>
 <html lang="en">
 
 <head>
- <meta charset="UTF-8">
- <meta name="viewport" content="width=device-width, initial-scale=1.0">
- <title>一覧表示</title>
- <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.5.0/css/bootstrap.min.css" integrity="sha384-9aIt2nRpC12Uk9gS9baDl411NQApFmC26EwAOH8WgZl5MYYxFfc+NcPb1dKGj7Sk" crossorigin="anonymous">
- <link rel="stylesheet" href="https://use.fontawesome.com/releases/v5.1.0/css/all.css" integrity="sha384-lKuwvrZot6UHsBSfcMvOkWwlCMgc0TaWr+30HWe3a4ltaBwTZhyTEggF5tJv8tbt" crossorigin="anonymous">
+  <meta charset="UTF-8">
+  <meta name="viewport" content="width=device-width, initial-scale=1.0">
+  <title>一覧表示</title>
+  <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.5.0/css/bootstrap.min.css" integrity="sha384-9aIt2nRpC12Uk9gS9baDl411NQApFmC26EwAOH8WgZl5MYYxFfc+NcPb1dKGj7Sk" crossorigin="anonymous">
+  <link rel="stylesheet" href="https://use.fontawesome.com/releases/v5.1.0/css/all.css" integrity="sha384-lKuwvrZot6UHsBSfcMvOkWwlCMgc0TaWr+30HWe3a4ltaBwTZhyTEggF5tJv8tbt" crossorigin="anonymous">
 </head>
 
 <body>
- <h1>一覧リスト</h1>
- <a href="./search_form.php">検索リンク</a>
- <a href="./insert_form.php">投稿リンク</a>
+  <h1>一覧リスト</h1>
+  <a href="./search_form.php">検索リンク</a>
+  <a href="./insert_form.php">投稿リンク</a>
 
- <?php if (is_string($_SESSION['auth_id'])) : ?>
-  <form action="./auth/login.php" method="get">
-   <button type="submit" class="btn btn-danger">ログイン</button>
-  </form>
-  <form action="./auth/signup_form.php" method="get">
-   <button type="submit" class="btn btn-danger">新規投稿</button>
-  </form>
- <?php else : ?>
-  <!--<form action="./auth/logout.php" method="post">-->
-  <form action="" method="post">
-   <button type="submit" name="logout" class="btn btn-danger">ログアウト</button>
-  </form>
- <?php endif; ?>
-
- <?php foreach ($lists as $list) : ?>
-  <div>
-   <td><?php echo $list['id']; ?></td>
-   <td><?php echo $list['title']; ?></td>
-   <td><?php echo $list['detail']; ?></td>
-   <form action="like.php" method="post" style="display:inline;">
-    <?php if (isLike($list['id'], $_SESSION['auth_id'])) : ?>
-     <button type="submit" class="btn p-0 border-0">
-      <input type="hidden" name="post_id" value="<?php echo $list['id']; ?>">
-      <i class="fas fa-heart fa-fw text-danger"></i>
-     </button>
-   </form>
+  <?php if (is_string($_SESSION['auth_id'])) : ?>
+    <form action="./auth/login.php" method="get">
+      <button type="submit" class="btn btn-danger">ログイン</button>
+    </form>
+    <form action="./auth/signup_form.php" method="get">
+      <button type="submit" class="btn btn-danger">新規投稿</button>
+    </form>
   <?php else : ?>
-   <form action="like.php" method="post" style="display:inline;">
-    <button type="submit" class="btn p-0 border-0">
-     <input type="hidden" name="post_id" value="<?php echo $list['id']; ?>">
-     <i class="fas fa-heart"></i>
-    </button>
-   <?php endif; ?>
-   </form>
-   <span>
-    <?php echo count(getLike($list['id'])); ?>
-   </span>
-   <td><button type="button" onclick="location.href='./update_form.php?id=<?php print($list['id']) ?>'">編集</button></td>
-  </div>
- <?php endforeach ?>
- <?php if (is_int($_SESSION['auth_id'])) : ?>
-  <button type="button" onclick="location.href='./mypage.php?id=<?php echo $_SESSION['auth_id'] ?>'">マイページ</button>
- <?php endif; ?>
+    <!--<form action="./auth/logout.php" method="post">-->
+    <form action="" method="post">
+      <button type="submit" name="logout" class="btn btn-danger">ログアウト</button>
+    </form>
+  <?php endif; ?>
+
+  <?php foreach ($lists as $list) : ?>
+    <div>
+      <td><?php echo $list['id']; ?></td>
+      <td><?php echo $list['title']; ?></td>
+      <td><?php echo $list['detail']; ?></td>
+      <form action="like.php" method="post" style="display:inline;">
+        <?php if (isLike($list['id'], $_SESSION['auth_id'])) : ?>
+          <button type="submit" class="btn p-0 border-0">
+            <input type="hidden" name="post_id" value="<?php echo $list['id']; ?>">
+            <i class="fas fa-heart fa-fw text-danger"></i>
+          </button>
+      </form>
+    <?php else : ?>
+      <form action="like.php" method="post" style="display:inline;">
+        <button type="submit" class="btn p-0 border-0">
+          <input type="hidden" name="post_id" value="<?php echo $list['id']; ?>">
+          <i class="fas fa-heart"></i>
+        </button>
+      <?php endif; ?>
+      </form>
+      <span>
+        <?php echo count(getLike($list['id'])); ?>
+      </span>
+      <td><button type="button" onclick="location.href='./update_form.php?id=<?php print($list['id']) ?>'">編集</button></td>
+    </div>
+  <?php endforeach ?>
+  <?php if (is_int($_SESSION['auth_id'])) : ?>
+    <button type="button" onclick="location.href='./mypage.php?id=<?php echo $_SESSION['auth_id'] ?>'">マイページ</button>
+  <?php endif; ?>
 </body>
 
 </html>
--- mypage.php
+++ mypage.php
@@ -6,7 +6,8 @@ include('util.php');
 auth_check('./auth/login.php');
 $db = dbConnect();
 
-$sql = 'SELECT * from posts 
+//$sql = 'SELECT * from posts 
+$sql = 'SELECT posts.* from posts 
 inner join users 
 on posts.user_id = users.id
 where users.id=:user_id';
@@ -14,9 +15,50 @@ where users.id=:user_id';
 $stmt = $db->prepare($sql);
 $stmt->bindValue(':user_id', $_GET['id'], PDO::PARAM_INT);
 $stmt->execute();
-$results = $stmt->fetchAll(PDO::FETCH_ASSOC);
-foreach ($results as $result) {
- $result = sanitize($result);
- echo $result['title'] . '<br>';
- echo $result['detail'] . ' ';
-}
+$lists = $stmt->fetchAll(PDO::FETCH_ASSOC);
+// 基本テキストフォームはないのでXSS対策はやる必要ないかも
+$lists = sanitize($lists);
+//var_dump($lists);
+?>
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+ <meta charset="UTF-8">
+ <meta name="viewport" content="width=device-width, initial-scale=1.0">
+ <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.5.0/css/bootstrap.min.css" integrity="sha384-9aIt2nRpC12Uk9gS9baDl411NQApFmC26EwAOH8WgZl5MYYxFfc+NcPb1dKGj7Sk" crossorigin="anonymous">
+ <link rel="stylesheet" href="https://use.fontawesome.com/releases/v5.1.0/css/all.css" integrity="sha384-lKuwvrZot6UHsBSfcMvOkWwlCMgc0TaWr+30HWe3a4ltaBwTZhyTEggF5tJv8tbt" crossorigin="anonymous">
+ <title>マイページ</title>
+</head>
+
+<body>
+ <?php foreach ($lists as $list) : ?>
+  <div>
+   <td><?php echo $list['id']; ?></td>
+   <td><?php echo $list['title']; ?></td>
+   <td><?php echo $list['detail']; ?></td>
+   <form action="like.php" method="post" style="display:inline;">
+    <?php if (isLike($list['id'], $_SESSION['auth_id'])) : ?>
+     <button type="submit" class="btn p-0 border-0">
+      <input type="hidden" name="post_id" value="<?php echo $list['id']; ?>">
+      <i class="fas fa-heart fa-fw text-danger"></i>
+     </button>
+   </form>
+  <?php else : ?>
+   <form action="like.php" method="post" style="display:inline;">
+    <button type="submit" class="btn p-0 border-0">
+     <input type="hidden" name="post_id" value="<?php echo $list['id']; ?>">
+     <i class="fas fa-heart"></i>
+    </button>
+   <?php endif; ?>
+   </form>
+   <span>
+    <?php echo count(getLike($list['id'])); ?>
+   </span>
+   <td><button type="button" onclick="location.href='./update_form.php?id=<?php print($list['id']) ?>'">編集</button></td>
+  </div>
+ <?php endforeach ?>
+ <a href="list.php">トップページへ</a>
+</body>
+
+</html>
