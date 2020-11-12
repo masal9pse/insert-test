9ec8abd70096ee742fde09abff6e83142cafd711
masal9pse <returnymgstokh@icloud.com>
2020-11-13 08:45:58 +0900

    トレイトを無理矢理使用

--- Classes/Function/SearchClass.php
+++ Classes/Function/SearchClass.php
@@ -1,15 +1,17 @@
 <?php
-require_once dirname(__FILE__) . '/../UtilClass.php';
+require_once dirname(__FILE__) . '/../TraitUtilClass.php';
 
-class SearchClass extends UtilClass
+class SearchClass
 {
-  public function AllSearch()
-  {
-    if (!empty($_GET['tags'] && $_GET['search'] && $_GET['category'])) {
-      list($tag_binds, $whereSql) = $this->tagWhere();
-      // explodeが使えるかチェック
-      //var_dump($whereSql);
-      $sql = "SELECT distinct posts.*
+ use TraitUtilClass;
+
+ public function AllSearch()
+ {
+  if (!empty($_GET['tags'] && $_GET['search'] && $_GET['category'])) {
+   list($tag_binds, $whereSql) = $this->tagWhere();
+   // explodeが使えるかチェック
+   //var_dump($whereSql);
+   $sql = "SELECT distinct posts.*
               FROM posts
               LEFT JOIN post_category
               ON posts.id = post_category.post_id
@@ -23,29 +25,29 @@ class SearchClass extends UtilClass
               AND tags.tag IN ($whereSql)  
               AND (posts.title like :title OR posts.detail like :detail )";
 
-      var_dump($sql);
-      $db = $this->dbConnect();
-      $stmt = $db->prepare($sql);
-      $stmt->bindValue(':category', $_GET['category'], PDO::PARAM_STR);
+   var_dump($sql);
+   $db = $this->dbConnect();
+   $stmt = $db->prepare($sql);
+   $stmt->bindValue(':category', $_GET['category'], PDO::PARAM_STR);
 
-      // タグ検索 $tag_bindsのキーと$whereSqlは同じ
-      $this->tagBinds($tag_binds, $stmt);
+   // タグ検索 $tag_bindsのキーと$whereSqlは同じ
+   $this->tagBinds($tag_binds, $stmt);
 
-      $stmt->bindValue(':title', "%{$_GET['search']}%", PDO::PARAM_STR);
-      $stmt->bindValue(':detail', "%{$_GET['search']}%", PDO::PARAM_STR);
-      $this->queryPost($stmt);
-    }
+   $stmt->bindValue(':title', "%{$_GET['search']}%", PDO::PARAM_STR);
+   $stmt->bindValue(':detail', "%{$_GET['search']}%", PDO::PARAM_STR);
+   $this->queryPost($stmt);
   }
+ }
 
-  // tagとカテゴリーの絞り込み検索
-  public function tagCategorySearch()
-  {
-    if (!empty($_GET['tags'] && $_GET['category']) && empty($_GET['search'])) {
+ // tagとカテゴリーの絞り込み検索
+ public function tagCategorySearch()
+ {
+  if (!empty($_GET['tags'] && $_GET['category']) && empty($_GET['search'])) {
 
-      list($tag_binds, $whereSql) = $this->tagWhere();
-      var_dump($whereSql);
+   list($tag_binds, $whereSql) = $this->tagWhere();
+   var_dump($whereSql);
 
-      $sql = "SELECT distinct posts.*
+   $sql = "SELECT distinct posts.*
               FROM posts
                 INNER JOIN post_category
                 ON posts.id = post_category.post_id
@@ -58,23 +60,23 @@ class SearchClass extends UtilClass
               WHERE  categories.category = :category
                 AND tags.tag IN ($whereSql)";
 
-      var_dump($sql);
-      $db = $this->dbConnect();
-      $stmt = $db->prepare($sql);
-      $stmt->bindValue(':category', $_GET['category'], PDO::PARAM_STR);
-      // タグ検索 $tag_bindsのキーと$whereSqlは同じ
-      $this->tagBinds($tag_binds, $stmt);
-      $this->queryPost($stmt);
-    }
+   var_dump($sql);
+   $db = $this->dbConnect();
+   $stmt = $db->prepare($sql);
+   $stmt->bindValue(':category', $_GET['category'], PDO::PARAM_STR);
+   // タグ検索 $tag_bindsのキーと$whereSqlは同じ
+   $this->tagBinds($tag_binds, $stmt);
+   $this->queryPost($stmt);
   }
+ }
 
-  public function tagTextSearch()
-  {
-    if (!empty($_GET['tags'] && $_GET['search']) && empty($_GET['category'])) {
-      list($tag_binds, $whereSql) = $this->tagWhere();
-      var_dump($whereSql);
+ public function tagTextSearch()
+ {
+  if (!empty($_GET['tags'] && $_GET['search']) && empty($_GET['category'])) {
+   list($tag_binds, $whereSql) = $this->tagWhere();
+   var_dump($whereSql);
 
-      $sql = "SELECT distinct posts.*
+   $sql = "SELECT distinct posts.*
               FROM posts
                 INNER JOIN post_category
                 ON posts.id = post_category.post_id
@@ -88,78 +90,78 @@ class SearchClass extends UtilClass
                 AND posts.title LIKE :title
                 OR posts.detail LIKE :detail";
 
-      var_dump($sql);
-      $db = $this->dbConnect();
-      $stmt = $db->prepare($sql);
-      // タグ検索 $tag_bindsのキーと$whereSqlは同じ
-      $this->tagBinds($tag_binds, $stmt);
-      $stmt->bindValue(':title', '%' . $_GET["search"] . '%', PDO::PARAM_STR);
-      $stmt->bindValue(':detail', '%' . $_GET["search"] . '%', PDO::PARAM_STR);
-      $this->queryPost($stmt);
-    }
+   var_dump($sql);
+   $db = $this->dbConnect();
+   $stmt = $db->prepare($sql);
+   // タグ検索 $tag_bindsのキーと$whereSqlは同じ
+   $this->tagBinds($tag_binds, $stmt);
+   $stmt->bindValue(':title', '%' . $_GET["search"] . '%', PDO::PARAM_STR);
+   $stmt->bindValue(':detail', '%' . $_GET["search"] . '%', PDO::PARAM_STR);
+   $this->queryPost($stmt);
   }
+ }
 
-  public function tagSearch()
-  {
-    if (!empty($_GET['tags']) && empty($_GET['search']) && empty($_GET['category'])) {
-      $first_sql = "SELECT distinct p.*
+ public function tagSearch()
+ {
+  if (!empty($_GET['tags']) && empty($_GET['search']) && empty($_GET['category'])) {
+   $first_sql = "SELECT distinct p.*
                     FROM post_tag pt, posts p, tags t
                     WHERE pt.tag_id = t.id
                       AND (t.tag IN (";
 
-      $second_sql = "AND p.id = pt.post_id";
+   $second_sql = "AND p.id = pt.post_id";
 
-      list($tag_binds, $whereSql) = $this->tagWhere();
+   list($tag_binds, $whereSql) = $this->tagWhere();
 
-      $sql = $first_sql . $whereSql . '))' . ' ' .  $second_sql;
-      //$sql .= $whereSql;
-      var_dump($sql);
-      $db = $this->dbConnect();
-      $stmt = $db->prepare($sql);
-      $this->tagBinds($tag_binds, $stmt);
-      $this->queryPost($stmt);
-    }
+   $sql = $first_sql . $whereSql . '))' . ' ' .  $second_sql;
+   //$sql .= $whereSql;
+   var_dump($sql);
+   $db = $this->dbConnect();
+   $stmt = $db->prepare($sql);
+   $this->tagBinds($tag_binds, $stmt);
+   $this->queryPost($stmt);
   }
+ }
 
-  //exit;
-  // カテゴリーのみが入力されている条件
-  public function categorySearch()
-  {
-    if (!empty($_GET['category'] && empty($_GET['search']) && empty($_GET['tags']))) {
-      $sql = 'SELECT * FROM posts 
+ //exit;
+ // カテゴリーのみが入力されている条件
+ public function categorySearch()
+ {
+  if (!empty($_GET['category'] && empty($_GET['search']) && empty($_GET['tags']))) {
+   $sql = 'SELECT * FROM posts 
               LEFT JOIN post_category 
               ON posts.id = post_category.post_id
               LEFT JOIN categories
               ON categories.id = post_category.category_id
               WHERE categories.category = :category';
 
-      var_dump($sql);
-      $db = $this->dbConnect();
-      $stmt = $db->prepare($sql);
-      $stmt->bindValue(':category', $_GET["category"], PDO::PARAM_STR);
-      $this->queryPost($stmt);
-    }
+   var_dump($sql);
+   $db = $this->dbConnect();
+   $stmt = $db->prepare($sql);
+   $stmt->bindValue(':category', $_GET["category"], PDO::PARAM_STR);
+   $this->queryPost($stmt);
   }
+ }
 
 
-  public function textSearch()
-  {
-    if (!empty($_GET['search']) && empty($_GET['category']) && empty($_GET['tags'])) {
-      $sql = 'SELECT * FROM posts 
+ public function textSearch()
+ {
+  if (!empty($_GET['search']) && empty($_GET['category']) && empty($_GET['tags'])) {
+   $sql = 'SELECT * FROM posts 
               WHERE posts.title LIKE :title OR posts.detail LIKE :detail';
 
-      $db = $this->dbConnect();
-      $stmt = $db->prepare($sql);
-      $stmt->bindValue(':title', '%' . $_GET["search"] . '%', PDO::PARAM_STR);
-      $stmt->bindValue(':detail', '%' . $_GET["search"] . '%', PDO::PARAM_STR);
-      $this->queryPost($stmt);
-    }
+   $db = $this->dbConnect();
+   $stmt = $db->prepare($sql);
+   $stmt->bindValue(':title', '%' . $_GET["search"] . '%', PDO::PARAM_STR);
+   $stmt->bindValue(':detail', '%' . $_GET["search"] . '%', PDO::PARAM_STR);
+   $this->queryPost($stmt);
   }
+ }
 
-  public function categoryTextSearch()
-  {
-    if (!empty($_GET['category'] && $_GET['search']) && empty($_GET['tags'])) {
-      $sql = 'SELECT distinct posts.* FROM posts 
+ public function categoryTextSearch()
+ {
+  if (!empty($_GET['category'] && $_GET['search']) && empty($_GET['tags'])) {
+   $sql = 'SELECT distinct posts.* FROM posts 
               LEFT JOIN post_category 
               ON posts.id = post_category.post_id
               LEFT JOIN categories
@@ -168,42 +170,42 @@ class SearchClass extends UtilClass
               AND (posts.title like :title 
               OR posts.detail like :detail)';
 
-      var_dump($sql);
-      $db = $this->dbConnect();
-      $stmt = $db->prepare($sql);
-      $stmt->bindValue(':category', $_GET['category'], PDO::PARAM_STR);
-      $stmt->bindValue(':title', '%' . $_GET['search'] . '%', PDO::PARAM_STR);
-      $stmt->bindValue(':detail', '%' . $_GET['search'] . '%', PDO::PARAM_STR);
-      //var_dump($stmt); 
-      $this->queryPost($stmt);
-    }
+   var_dump($sql);
+   $db = $this->dbConnect();
+   $stmt = $db->prepare($sql);
+   $stmt->bindValue(':category', $_GET['category'], PDO::PARAM_STR);
+   $stmt->bindValue(':title', '%' . $_GET['search'] . '%', PDO::PARAM_STR);
+   $stmt->bindValue(':detail', '%' . $_GET['search'] . '%', PDO::PARAM_STR);
+   //var_dump($stmt); 
+   $this->queryPost($stmt);
   }
+ }
 
-  public function AllNotSearch()
-  {
-    if (empty($_GET['search']) && empty($_GET['category']) && empty($_GET['tags'])) {
-      echo '結果は０件です';
-    }
+ public function AllNotSearch()
+ {
+  if (empty($_GET['search']) && empty($_GET['category']) && empty($_GET['tags'])) {
+   echo '結果は０件です';
   }
+ }
 
-  private function tagWhere()
-  {
-    $tag_where = [];
-    $tag_binds = [];
-    foreach ($_GET['tags'] as $key =>  $tag) {
-      $tag_where[] = ":tag" . $key;
-      $tag_binds[":tag" . $key] = $tag;
-    }
-    $whereSql = implode(' , ', $tag_where);
-    var_dump($whereSql);
-    return [$tag_binds, $whereSql];
+ private function tagWhere()
+ {
+  $tag_where = [];
+  $tag_binds = [];
+  foreach ($_GET['tags'] as $key =>  $tag) {
+   $tag_where[] = ":tag" . $key;
+   $tag_binds[":tag" . $key] = $tag;
   }
+  $whereSql = implode(' , ', $tag_where);
+  var_dump($whereSql);
+  return [$tag_binds, $whereSql];
+ }
 
-  private function tagBinds($tag_binds, $stmt)
-  {
-    foreach ($tag_binds as $key => $val) {
-      var_dump($key);
-      $stmt->bindValue($key, $val, PDO::PARAM_STR);
-    }
+ private function tagBinds($tag_binds, $stmt)
+ {
+  foreach ($tag_binds as $key => $val) {
+   var_dump($key);
+   $stmt->bindValue($key, $val, PDO::PARAM_STR);
   }
+ }
 }
---
+++ Classes/TraitUtilClass.php
@@ -0,0 +1,70 @@
+<?php
+trait TraitUtilClass
+{
+ protected $table_name;
+ protected $sort;
+
+ function dbConnect()
+ {
+  try {
+   $db = new PDO('pgsql:dbname=offshoa_db;host=127.0.0.1;port=5432;', 'yamamotohiroto', '');
+   //echo '接続成功です';
+  } catch (PDOException $e) {
+   ini_set('display_errors', "On");
+   echo $e . 'エラーです';
+  }
+  return $db;
+ }
+
+ function sanitize($inputs)
+ {
+  if (is_array($inputs)) {
+   $_input = array();
+   foreach ($inputs as $key => $val) {
+    if (is_array($val)) {
+     $key = htmlspecialchars($key, ENT_QUOTES, 'UTF-8');
+     $_input[$key] = $this->sanitize($val);
+    } else {
+     $key = htmlspecialchars($key, ENT_QUOTES, 'UTF-8');
+     $_input[$key] = htmlspecialchars($val, ENT_QUOTES, 'UTF-8');
+    }
+   }
+   return $_input;
+  } else {
+   return htmlspecialchars($inputs, ENT_QUOTES, 'UTF-8');
+  }
+ }
+
+ public function saveCsrf()
+ {
+  $toke_byte = openssl_random_pseudo_bytes(16);
+  $csrf_token = bin2hex($toke_byte);
+  // 生成したトークンをセッションに保存します
+  $_SESSION['csrf_token'] = $csrf_token;
+ }
+
+ function auth_check($redirectPath)
+ {
+  if (!isset($_SESSION['name'])) {
+   // ログインする前にそのページのurlを取得する
+   $_SESSION['return'] = $_SERVER["REQUEST_URI"];
+   header("Location: $redirectPath");
+   exit();
+  }
+ }
+
+ function empty_check($key, $name)
+ {
+  if (!empty($key[$name])) {
+   print($key[$name]);
+  }
+ }
+
+ protected function queryPost($stmt)
+ {
+  $stmt->execute();
+  $results = $stmt->fetchAll(PDO::FETCH_ASSOC);
+  $results = $this->sanitize($results);
+  var_dump($results);
+ }
+}
