7f42d47a27c43d94323485474099508e292a8cda
masal9pse <returnymgstokh@icloud.com>
2020-11-15 05:09:46 +0900

    いいね機能実装完了

--- Api/Like.php
+++ Api/Like.php
@@ -1,31 +1,20 @@
 <?php
 session_start();
 // APIを実行するファイルにheaderを定義する
-//header('Access-Control-Allow-Origin: *');
-//header('Content-Type: application/json');
-//header('Access-Control-Allow-Methods: POST');
-//header('Access-Control-Allow-Headers: Access-Control-Allow-Headers,Content-Type,Access-Control-Allow-Methods, Authorization, X-Requested-With');
+header('Access-Control-Allow-Origin: *');
+header('Content-Type: application/json');
+header('Access-Control-Allow-Methods: POST');
+header('Access-Control-Allow-Headers: Access-Control-Allow-Headers,Content-Type,Access-Control-Allow-Methods, Authorization, X-Requested-With');
 require('../Classes/Function/LikeClass.php');
 
 $likeApi = new LikeClass;
 
-//$data = json_decode(file_get_contents("php://input"));
-//$data = json_decode($_POST['post_id']);
-//$data2 = json_decode($_SESSION['auth_id']);
-
-//$likeApi->post_id = $data->post_id;
 $likeApi->post_id = $_POST['post_id'];
-//$likeApi->user_id = $data2->user_id;
-//$likeApi->user_id = $_SESSION['auth_id'];
-//$likeApi->user_id = $_POST['auth_id'];
-
 
 if (!$likeApi->isLike($likeApi->post_id, $_SESSION['auth_id'])) {
  $likeApi->addLikeApi();
  echo count($likeApi->getLike($likeApi->post_id));
- echo json_encode(array('message' => 'いいねしました'));
 } else {
  $likeApi->rmLikeApi();
  echo count($likeApi->getLike($likeApi->post_id));
- echo json_encode(array('message' => 'いいねを削除しました'));
 }
--- like.js
+++ like.js
@@ -10,7 +10,6 @@ $(function() {
   $.ajax({
    type: 'POST',
    url: './Api/Like.php', //post送信を受けとるphpファイル
-   //data: {postd: likePostId, userId: likeUserId} //{キー:投稿ID}
    data: {post_id: likePostId} //{キー:投稿ID}
   }).done(function(data) {
    console.log('Ajax Success');
@@ -27,13 +26,6 @@ $(function() {
   }).fail(function(msg) {
    console.log('Ajax Error');
    console.log(this.data);
-   $this.children('span').html(data);
-   // いいね取り消しのスタイル
-   $this.children('i').toggleClass('far'); //空洞ハート
-   // いいね押した時のスタイル
-   $this.children('i').toggleClass('fas'); //塗りつぶしハート
-   $this.children('i').toggleClass('active');
-   $this.toggleClass('active');
   });
  });
 });
