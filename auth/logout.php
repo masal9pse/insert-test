6bffe8dd7daf7336ebe6abc6aa02aaa16d80a9d9
masal9pse <returnymgstokh@icloud.com>
2020-11-29 08:57:58 +0900

    [fix]ログアウトの際のリダイレクト先のパスのエラーを修正

--- auth/logout.php
+++ auth/logout.php
@@ -6,4 +6,4 @@ require_once __DIR__ . '/../vendor/autoload.php';
 use App\Controllers\AuthController;
 
 $auth = new AuthController;
-$auth->logout($_SESSION, $redirect);
+$auth->logout($_SESSION, "../views/index.php");
