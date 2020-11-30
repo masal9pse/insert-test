69a55475c2a8ee35fec1370346c1e41cece71f21
masal9pse <returnymgstokh@icloud.com>
2020-11-30 10:25:36 +0900

    [new finding] phpunitコマンドの簡略化

--- app/Controllers/UtilController.php
+++ app/Controllers/UtilController.php
@@ -15,8 +15,6 @@ class UtilController extends UtilModel
     'test_user',
     'secret'
    );
-   //$db = new PDO('pgsql:dbname=offshoa_db;host=127.0.0.1;port=5432;', 'yamamotohiroto', '');
-   //echo '接続成功です';
   } catch (PDOException $e) {
    echo $e . 'err';
   }
