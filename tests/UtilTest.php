1c11bf84326f787a7e005d035f93b1d07a80712c
masal9pse <returnymgstokh@icloud.com>
2020-12-02 09:51:57 +0900

    dbconnect path not using mock

--- tests/UtilTest.php
+++ tests/UtilTest.php
@@ -20,8 +20,22 @@ class UtilTest extends TestCase
    ->willReturn($db);
 
   $result = $mock->dbconnect();
-  var_dump($result);
+  //var_dump($result);
   $this->assertIsObject($result);
+  $this->assertEquals($db, $result);
+ }
+
+ // コンテナ内に必ず入ること
+ public function testDbConnectNotUsingMock()
+ {
+  $util = new UtilController;
+  $result = $util->dbConnect();
+  $db = new PDO(
+   'pgsql:host=db;dbname=offshoa_db;',
+   'test_user',
+   'secret',
+  );
+  var_dump($result);
   $this->assertEquals($db, $result);
  }
 }
